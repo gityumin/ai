ff704121d0c05cc0af8c79ab8be6d504e14acd91
gityumin <qkanno5693@gmail.com>
2019-02-07 05:46:19 +0000

    flags add

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import numpy as np
-for step in xrange(8):
-    print step
-
+flags = tf.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string("data","hello","test")
 a = tf.placeholder("float")
 b = tf.placeholder("float")
 y = tf.multiply(a,b)
 sess = tf.Session()
 print sess.run(y,feed_dict={a:4,b:5})
+print FLAGS.data
