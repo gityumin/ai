aec0ee3a04d7ca865780ed87951fbd4086dedb2c
gityumin <qkanno5693@gmail.com>
2019-02-07 06:02:06 +0000

    complete2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 import numpy as np
+
 flags = tf.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string("data","hello","test")
+flags.DEFINE_string("data2","world","test")
 a = tf.placeholder("float")
 b = tf.placeholder("float")
 y = tf.multiply(a,b)
 sess = tf.Session()
 print sess.run(y,feed_dict={a:4,b:5})
 print FLAGS.data
+print FLAGS.data2
 print "hello !!"
