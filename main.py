ab956f19186f6cd47912baaf9bd2d650bc3a5b65
gityumin <qkanno5693@gmail.com>
2019-02-07 05:53:40 +0000

    complete

--- main.py
+++ main.py
@@ -8,8 +8,5 @@ b = tf.placeholder("float")
 y = tf.multiply(a,b)
 sess = tf.Session()
 print sess.run(y,feed_dict={a:4,b:5})
-<<<<<<< HEAD
 print FLAGS.data
-=======
 print "hello !!"
->>>>>>> c285babdeb662d4034a01ff5f5463d7f61876bfb
